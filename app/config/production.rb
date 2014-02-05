c4d5a30a1fa68e2b2f9f3df18d80e22020d1cd92
Marc Perrin-Pelletier <marc@majorclass.fr>
2014-02-05 15:17:29 +0100

    Changements divers
    - Nouveau .htaccess compatible avec capifony
    - Chemin édité dans production.rb et staging.rb
    - {{ error }} ajouté dans la view du login

--- app/config/production.rb
+++ app/config/production.rb
@@ -1,5 +1,5 @@
 server 'majorclass.fr', :app, :web, :primary => true
 set :domain,      "majorclass.fr"
-set :deploy_to,   "/home/majorcla/var/www/majordesk/production/#{domain}"
+set :deploy_to,   "/home/majorcla/public_html/majordesk/production/#{domain}"
 set :branch, "master"
 set :parameters_file, "parameters_production.yml"
--- app/config/staging.rb
+++ app/config/staging.rb
@@ -1,5 +1,5 @@
 server 'staging.majorclass.fr', :app, :web, :primary => true
 set :domain,      "staging.majorclass.fr"
-set :deploy_to,   "/home/majorcla/var/www/majordesk/staging/staging.majorclass.fr"
+set :deploy_to,   "/home/majorcla/public_html/majordesk/staging/staging.majorclass.fr"
 set :branch, "master"
 set :parameters_file, "parameters_staging.yml"
